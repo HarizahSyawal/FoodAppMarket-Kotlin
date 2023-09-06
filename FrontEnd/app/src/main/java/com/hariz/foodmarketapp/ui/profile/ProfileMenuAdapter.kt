85e36783bb73a4da15b551dbd98d7b94dbba1314
HarizahSyawal <37772347+HarizahSyawal@users.noreply.github.com>
2023-09-06 18:23:19 +0800

    New Changes :
    feat(Profile Tab UI)

---
+++ FrontEnd/app/src/main/java/com/hariz/foodmarketapp/ui/profile/ProfileMenuAdapter.kt
@@ -0,0 +1,45 @@
+package com.hariz.foodmarketapp.ui.profile
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.hariz.foodmarketapp.R
+import com.hariz.foodmarketapp.model.dummy.ProfileMenuModel
+import kotlinx.android.synthetic.main.item_home_horizontal.view.tvTitle
+
+class ProfileMenuAdapter (
+    private val listData : List<ProfileMenuModel>,
+    private val itemAdapterCallback : ItemAdapterCallback,
+) : RecyclerView.Adapter<ProfileMenuAdapter.ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.item_menu_profile, parent, false)
+        return ViewHolder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(listData[position], itemAdapterCallback)
+    }
+
+    override fun getItemCount(): Int {
+        return listData.size
+    }
+
+    class ViewHolder (itemView:View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(data : ProfileMenuModel, itemAdapterCallback: ItemAdapterCallback) {
+            itemView.apply {
+                tvTitle.text = data.title
+
+                itemView.setOnClickListener { itemAdapterCallback.onClick(it, data) }
+            }
+        }
+    }
+
+    interface ItemAdapterCallback {
+        fun onClick(v: View, data:ProfileMenuModel)
+    }
+
+}
--- FrontEnd/app/src/main/java/com/hariz/foodmarketapp/ui/profile/account/ProfileAccountFragment.kt
+++ FrontEnd/app/src/main/java/com/hariz/foodmarketapp/ui/profile/account/ProfileAccountFragment.kt
@@ -5,9 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.hariz.foodmarketapp.R
+import com.hariz.foodmarketapp.model.dummy.ProfileMenuModel
+import com.hariz.foodmarketapp.ui.profile.ProfileMenuAdapter
+import kotlinx.android.synthetic.main.fragment_profile_account.*
 
-class ProfileAccountFragment : Fragment() {
+class ProfileAccountFragment : Fragment(), ProfileMenuAdapter.ItemAdapterCallback {
+
+    private var menuArrayList: ArrayList<ProfileMenuModel> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,4 +24,28 @@ class ProfileAccountFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile_account, container, false)
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        initDataDummy()
+
+        var adapter = ProfileMenuAdapter(menuArrayList, this)
+        var layoutManager : RecyclerView.LayoutManager = LinearLayoutManager(activity)
+        rcList.layoutManager = layoutManager
+        rcList.adapter = adapter
+    }
+
+    fun initDataDummy() {
+        menuArrayList = ArrayList()
+        menuArrayList.add(ProfileMenuModel("Edit Profile"))
+        menuArrayList.add(ProfileMenuModel("Home Address"))
+        menuArrayList.add(ProfileMenuModel("Security"))
+        menuArrayList.add(ProfileMenuModel("Payments"))
+    }
+
+    override fun onClick(v: View, data: ProfileMenuModel) {
+        Toast.makeText(context, "ini menu yang kamu klik "+data.title, Toast.LENGTH_SHORT).show()
+    }
+
 }
